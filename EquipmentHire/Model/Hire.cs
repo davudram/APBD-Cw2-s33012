2a642bee30987d9b433dc49bb4006ad79b49c570
Davud Ramazanov <davudramazanov2007@gmail.com>
2026-03-21 17:32:15 +0100

    Add User, Equipment, Projector, Laptop, Camera and Hire models

---
+++ EquipmentHire/Model/Hire.cs
@@ -0,0 +1,10 @@
+﻿namespace EquipmentHire.Model;
+
+public class Hire(User hUser, Equipment hEquipment, DateTime hHireDate, int hDayOfHire, bool hIsReturnOnTime)
+{
+    public User User { get; set; } = hUser;
+    public Equipment Equipment { get; set; } = hEquipment;
+    public DateTime DateOfHire { get; set; } = hHireDate;
+    public int DayOfHire { get; set; } = hDayOfHire;
+    public bool IsReturnedOnTime { get; set; } = hIsReturnOnTime;
+}
---
+++ EquipmentHire/Model/User.cs
@@ -0,0 +1,13 @@
+﻿using EquipmentHire.Enums;
+
+namespace EquipmentHire.Model;
+
+public class User(string uName, string uSurname, TypeUser uType)
+{
+    private static int _userId = 1;
+
+    public int UserId { get; set; } = _userId++;
+    public string Name { get; set; } = uName;
+    public string Surname { get; set; } = uSurname;
+    public TypeUser TypeUser { get; set; } = uType;
+}
