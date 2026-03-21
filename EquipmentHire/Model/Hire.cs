20ce4fc1817b57acb598947692afb8f0fb0b4808
Davud Ramazanov <davudramazanov2007@gmail.com>
2026-03-21 22:59:41 +0100

    Update Hire model

--- EquipmentHire/Model/Hire.cs
+++ EquipmentHire/Model/Hire.cs
@@ -1,10 +1,16 @@
 ﻿namespace EquipmentHire.Model;
 
-public class Hire(User hUser, Equipment hEquipment, DateTime hHireDate, int hDayOfHire, bool hIsReturnOnTime)
+public class Hire(User hUser, Equipment hEquipment, DateTime hHireStart, DateTime hHireExcepted, int hDayOfHire, bool hIsReturnOnTime)
 {
+    private static int _hireId = 1;
+    
+    public int HireId { get; set; } = _hireId++;
     public User User { get; set; } = hUser;
     public Equipment Equipment { get; set; } = hEquipment;
-    public DateTime DateOfHire { get; set; } = hHireDate;
+    public DateTime StartOfHire { get; set; } = hHireStart;
+    public DateTime ExceptedOfHire { get; set; } = hHireExcepted;
     public int DayOfHire { get; set; } = hDayOfHire;
+    public DateTime? ActualReturnDate { get; set; }
+    public decimal? FineCharges { get; set; }
     public bool IsReturnedOnTime { get; set; } = hIsReturnOnTime;
 }
